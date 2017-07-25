f4f82b42b1f2dd732c90cf5abd6a45a02b40f038
= <aillieo@163.com>
2017-07-25 19:06:11 +0800

    update

---
+++ gl-practice/PracticeCode/BasicColor.cpp
@@ -0,0 +1,14 @@
+#include "SimpleTransform3D.h"
+
+#ifdef WIN32
+#include <windows.h>
+#elif __APPLE__
+
+#endif
+
+#include "../Utils/Shader.h"
+
+#include "../Utils/TextureLoader.h"
+#include "../SceneManager.h"
+
+
