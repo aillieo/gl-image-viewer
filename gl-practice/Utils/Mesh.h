26ddc3711e95d77f3972a43c055356265828338c
aillieo <aillieo@163.com>
2017-08-07 21:48:37 +0800

    update

--- gl-practice/Utils/Mesh.h
+++ gl-practice/Utils/Mesh.h
@@ -9,18 +9,63 @@
 #include "glm/gtc/matrix_transform.hpp"
 
 #include "../Utils/GLMethod.h"
+#include "../Utils/Shader.h"
+
+using std::vector;
+using std::string;
+
+struct Vertex {
+    // position
+    glm::vec3 Position;
+    // normal
+    glm::vec3 Normal;
+    // texCoords
+    glm::vec2 TexCoords;
+    // tangent
+    glm::vec3 Tangent;
+    // bitangent
+    glm::vec3 Bitangent;
+};
 
 
+struct Texture {
+    unsigned int id;
+    string type;
+    aiString path;
+};
 
-class Mesh
-{
+
+class Mesh {
 public:
-
-
-
+    /*  Mesh Data  */
+    vector<Vertex> vertices;
+    vector<unsigned int> indices;
+    vector<Texture> textures;
+    unsigned int VAO;
+    
+    // constructor
+    Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
+    {
+        this->vertices = vertices;
+        this->indices = indices;
+        this->textures = textures;
+        
+        // now that we have all the required data, set the vertex buffers and its attribute pointers.
+        setupMesh();
+    }
+    
+    // render the mesh
+    void Draw(Shader shader);
+    
 private:
-	
+    
+    /*  Render data  */
+    unsigned int VBO, EBO;
+    
+    // initializes VAO VBO EBO
+    void setupMesh();
 
 };
+
 
 #endif
---
+++ gl-practice/Utils/Model.h
@@ -0,0 +1,55 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "GL/glew.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "assimp/Importer.hpp"
+#include "assimp/scene.h"
+#include "assimp/postprocess.h"
+
+#include "../Utils/GLMethod.h"
+#include "../Utils/Mesh.h"
+#include "../Utils/Shader.h"
+
+using std::vector;
+using std::string;
+
+class Model
+{
+public:
+    /*  Model Data */
+    // stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
+    vector<Texture> textures_loaded;
+    
+    vector<Mesh> meshes;
+    string directory;
+    bool gammaCorrection;
+    
+    // constructor, expects a filepath to a 3D model.
+    Model(string const &path, bool gamma = false);
+    
+    // draws the model, and thus all its meshes
+    void Draw(Shader shader);
+    
+private:
+
+    // loads a model with supported ASSIMP extensions from file and stores the resulting meshes in the meshes vector.
+    void loadModel(string const &path);
+
+    
+    // processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
+    void processNode(aiNode *node, const aiScene *scene);
+    
+    Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+    
+    
+    // checks all material textures of a given type and loads the textures if they're not loaded yet.
+    // the required info is returned as a Texture struct.
+    vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, string typeName);
+    
+};
+
+#endif
