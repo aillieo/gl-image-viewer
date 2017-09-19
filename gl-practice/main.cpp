ef748015c9ab46c8d02c9a3edfa1a94b7e58379f
= <aillieo@163.com>
2017-09-19 18:24:34 +0800

    update

--- gl-practice/main.cpp
+++ gl-practice/main.cpp
@@ -41,7 +41,7 @@ int main()
 	}
 
 	//
-	SceneManager::sceneIndexMax = 92;
+	SceneManager::sceneIndexMax = 93;
 	SceneManager::index = SceneManager::sceneIndexMax;
 
 	while (!glfwWindowShouldClose(window))
